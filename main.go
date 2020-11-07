c159c7520a2894037d874237c36ed27502ab3d8b
Max Stanley <github@maxstanley.uk>
2020-11-07 21:27:27 +0000

    Lambda returns json required for proxy

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func helloHandler() (string, error) {
-	return "hello", nil
+func response(body string, statusCode int) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       string(body),
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+	}
+}
+
+func helloHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return response("Hello World", http.StatusOK), nil
 }
 
 func main() {
